75e262aef622f679e2c8f8061015a53bddc6cad2
Zachary Johnson <vepped@gmail.com>
2022-08-05 15:34:56 -0400

    Got something on the screen

--- react-weather-app/src/Weather/HelloWeather.js
+++ react-weather-app/src/Weather/HelloWeather.js
@@ -1,1 +1,7 @@
 // Test
+
+export default function(){
+    return(
+        <h1>Whats the Weather?</h1>
+    )
+}
--- react-weather-app/src/index.js
+++ react-weather-app/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HelloWeather from './Weather/HelloWeather';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   
+   <HelloWeather/>
   </React.StrictMode>
 );
 
