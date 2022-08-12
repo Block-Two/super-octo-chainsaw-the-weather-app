1185aecc1acdb88beac06b61cacec4e0357f3b5f
Zachary Turney <99896219+turneyz@users.noreply.github.com>
2022-08-11 19:28:06 -0700

    Corrected API weather information and getting user's local time with rounded minutes and seconds

--- react-weather-app/src/Weather-Status/status.js
+++ react-weather-app/src/Weather-Status/status.js
@@ -1,14 +1,59 @@
 let lat = 38.000975
 let lon = -121.334856
 
-let fetchWeather = function () {
-    fetch(`https://www.7timer.info/bin/api.pl?lon=${lon}&lat=${lat}&product=civil&output=json`)
+// let FetchWeather = function () {
+//     fetch(`https://www.7timer.info/bin/api.pl?lon=${lon}&lat=${lat}&product=civil&output=json`)
+//     .then((response) => response.json())
+//     .then((result) => {
+//         console.log(result.dataseries)
+//         console.log("Testing Weather Status")
+//         return (
+//             <h1>Testing Weather Status</h1>
+//             // <h1>{result}</h1>
+//         )
+//     })
+// }
+
+let date = new Date(); 
+
+function roundMinutes() {
+    let time = date.getHours() + ":00"
+    return time;
+}
+
+function convertedDate() {
+    let mm = date.getMonth() + 1; 
+    let dd = date.getDate();
+  
+    return [date.getFullYear(),
+        "-" + (mm>9 ? '' : '0') + mm,
+        "-" + (dd>9 ? '' : '0') + dd,
+        "T" + roundMinutes()
+    ].join('').toString();
+};
+convertedDate();
+//   console.log(convertedDate())
+
+
+let FetchWeather = function () {
+    fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FLos_Angeles`)
+    .then((response) => response.json())
     .then((result) => {
-        console.log(result)
+        // console.log(result)
+        let temp = convertedDate()
+        for (let i = 0; i < result.hourly.time.length; i++) {
+            if (result.hourly.time[i] === temp) {
+                // console.log(i)
+                // console.log(result.hourly.time[i])
+                console.log(result.hourly.temperature_2m[i] + " degrees")
+            }
+        }
+
         return (
-            <h1>{result}</h1>
+            // <img src={result.url}/>
+            <h1>Testing</h1>
         )
     })
 }
 
-export default fetchWeather
+export default FetchWeather
--- react-weather-app/src/index.js
+++ react-weather-app/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import fetchWeather from './Weather-Status/status.js';
+import FetchWeather from './Weather-Status/status.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-   <fetchWeather />
+   <FetchWeather />
   </React.StrictMode>
 );
 
